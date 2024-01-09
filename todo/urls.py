4d7c5d8818d4f6be0a58f0a06b282717f0110685
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2024-01-09 22:23:33 +0000

    Creating urls for all views

---
+++ todo/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from todo.views import (IndexView,
+                        TaskCreateView,
+                        TagListView,
+                        TagCreateView,
+                        TagUpdateView,
+                        TagDeleteView, TaskDoneView)
+
+urlpatterns = [
+    path('', IndexView.as_view(), name="index"),
+    path("tags/", TagListView.as_view(), name="tags_list"),
+    path("task/create/", TaskCreateView.as_view(), name="task_create"),
+    path("tags/create/", TagCreateView.as_view(), name="tag_create"),
+    path("tags/update/<int:pk>", TagUpdateView.as_view(), name="tag_update"),
+    path("tags/delete/<int:pk>", TagDeleteView.as_view(), name="tag_delete"),
+    path("task/done/<int:pk>", TaskDoneView.taskdone, name="tag_delete"),
+]
