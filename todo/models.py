a84a8d3696b235e0ccc33d2c34c5b83c1c6d1e1f
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2024-01-09 21:28:34 +0000

    Creating models for todo app

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Task(models.Model):
+    content = models.TextField(max_length=100)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, none=True)
+    done = models.BooleanField(default=False, null=True)
+    tags = models.ManyToManyField(Tag)
