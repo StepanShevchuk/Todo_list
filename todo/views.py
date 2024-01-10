a220a7b8228d096d4ba08bda6f7fc53260ab699c
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2024-01-10 08:52:59 +0000

    Create all views

--- todo/views.py
+++ todo/views.py
@@ -7,46 +7,49 @@ from django.views import generic
 from todo.models import Task, Tag
 
 
-class IndexView(LoginRequiredMixin, generic.ListView):
+class IndexView(generic.ListView):
     model = Task
     template_name = "todo/index.html"
 
 
-class TagListView(LoginRequiredMixin, generic.ListView):
+class TagListView(generic.ListView):
     model = Tag
     template_name = "todo/tag_list.html"
 
 
-class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
     template_name = "todo/task_create.html"
     success_url = reverse_lazy("todo:index")
 
 
-class TagCreateView(LoginRequiredMixin, generic.CreateView):
+class TagCreateView(generic.CreateView):
     model = Tag
     fields = "__all__"
     template_name = "todo/tag_create.html"
     success_url = reverse_lazy("todo:tags_list")
 
 
-class TagUpdateView(LoginRequiredMixin, generic.UpdateView):
+class TagUpdateView(generic.UpdateView):
     model = Tag
     fields = "__all__"
-    template_name = "todo/tag_update.html"
+    template_name = "todo/tag_create.html"
     success_url = reverse_lazy("todo:tags_list")
 
 
-class TagDeleteView(LoginRequiredMixin, generic.DeleteView):
+class TagDeleteView(generic.DeleteView):
     model = Tag
     template_name = "todo/tag_delete.html"
     success_url = reverse_lazy("todo:tags_list")
 
 
-class TaskDoneView(LoginRequiredMixin):
+class TaskDoneView:
     @classmethod
     def taskdone(cls, request, pk):
-        if request.method == "POST":
-            Task.objects.get(id=pk).done = not(Task.objects.get(id=pk).done)
-        return HttpResponseRedirect(reverse("todo:task_list"))
+        task = Task.objects.get(id=pk)
+        if task.done == 1:
+            task.done = 0
+        else:
+            task.done = 1
+        return HttpResponseRedirect(reverse_lazy("todo:tags_list"))
