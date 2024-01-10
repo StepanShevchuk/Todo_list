f5aded28f14610c0ae347ae21a6f6d20f97b3c12
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2024-01-10 20:47:32 +0000

    Adding ordering for task models

--- todo/models.py
+++ todo/models.py
@@ -4,10 +4,16 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     content = models.TextField(max_length=100)
     datetime = models.DateTimeField(auto_now_add=True)
-    deadline = models.DateTimeField(null=True, none=True)
-    done = models.BooleanField(default=False, null=True)
+    deadline = models.DateTimeField(null=True)
+    done = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        ordering = ["done", "-datetime"]
