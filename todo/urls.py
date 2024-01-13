afe6058924112107d075251510be3996384d3740
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2024-01-13 19:22:15 +0000

    Adding / to ends of urls

--- todo/urls.py
+++ todo/urls.py
@@ -12,9 +12,9 @@ urlpatterns = [
     path("tags/", TagListView.as_view(), name="tags_list"),
     path("task/create/", TaskCreateView.as_view(), name="task_create"),
     path("tags/create/", TagCreateView.as_view(), name="tag_create"),
-    path("tags/update/<int:pk>", TagUpdateView.as_view(), name="tag_update"),
-    path("tags/delete/<int:pk>", TagDeleteView.as_view(), name="tag_delete"),
-    path("task/done/<int:pk>", TaskDoneView.as_view(), name="task_done"),
+    path("tags/update/<int:pk>/", TagUpdateView.as_view(), name="tag_update"),
+    path("tags/delete/<int:pk>/", TagDeleteView.as_view(), name="tag_delete"),
+    path("task/done/<int:pk>/", TaskDoneView.as_view(), name="task_done"),
 ]
 
 app_name = "todo"
